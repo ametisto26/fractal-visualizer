c40f86124dc42d2baf5931b91ac29986e7d65dbe
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2026-05-12 20:16:36 +0900

    ズーム表示実装

--- mandelbrot/main.py
+++ mandelbrot/main.py
@@ -1,68 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 描画範囲
-xmin, xmax = -2.0, 1.0
-ymin, ymax = -1.5, 1.5
+def mandelbrot(cx, cy, scale=1.0, width=800, height=800, max_iter=100):
 
-# 解像度
-width, height = 1000, 1000
+    # 描画範囲
+    base_width = 3.0
+    base_height = 3.0
 
-# 最大反復回数
-max_iter = 100
+    x_width = base_width / scale
+    y_height = base_height / scale
 
-# 複素平面
-x = np.linspace(xmin, xmax, width)
-y = np.linspace(ymin, ymax, height)
+    xmin = cx - x_width / 2
+    xmax = cx + x_width / 2
+    ymin = cy - y_height / 2
+    ymax = cy + y_height / 2
 
-X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
+    # 解像度
+    # width, height：引数をそのまま使う
 
-# 初期値
-Z = np.zeros_like(C)
+    # 最大反復回数
+    # max_iter：引数をそのまま使う
 
-# 発散回数記録用
-divergence_step = np.zeros(C.shape, dtype=float)
+    # 複素平面
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
 
-# まだ発散していない点
-mask = np.ones(C.shape, dtype=bool)
+    X, Y = np.meshgrid(x, y)
+    C = X + 1j * Y
 
-for i in range(max_iter):
+    # 初期値
+    Z = np.zeros_like(C)
 
-    # 未発散点だけ更新
-    Z[mask] = Z[mask]**2 + C[mask]
+    # 発散回数記録用
+    divergence_step = np.zeros(C.shape, dtype=float)
 
-    # 発散判定
-    diverged = np.abs(Z) > 2
+    #初期化
+    divergence_step[:] = max_iter
 
-    # 今回新しく発散した点
-    newly_diverged = diverged & mask
+    # まだ発散していない点
+    mask = np.ones(C.shape, dtype=bool)
 
-    # 発散回数を記録
-    abs_z = np.abs(Z[newly_diverged])
+    for i in range(max_iter):
 
-    nu = i + 1 - np.log(np.log(abs_z)) / np.log(2)
+        # 未発散点だけ更新
+        Z[mask] = Z[mask]**2 + C[mask]
 
-    divergence_step[newly_diverged] = nu
+        # 発散判定
+        diverged = np.abs(Z) > 2
 
-    # 発散済みを除外
-    mask &= ~diverged
+        # 今回新しく発散した点
+        newly_diverged = diverged & mask
 
-# 描画
-plt.figure(figsize=(8, 8))
+        # 発散回数を記録
+        abs_z = np.abs(Z[newly_diverged]) + 1e-12
 
-plt.imshow(
-    divergence_step,
-    extent=[xmin, xmax, ymin, ymax],
-    cmap="twilight_shifted",
-    origin="lower",
-    interpolation="bicubic"
-)
+        nu = i + 1 - np.log(np.log(abs_z)) / np.log(2.0)
 
-plt.colorbar(label="Iterations")
+        divergence_step[newly_diverged] = nu
 
-plt.title("Mandelbrot Set")
-plt.xlabel("Re(c)")
-plt.ylabel("Im(c)")
+        # 発散済みを除外
+        mask &= ~diverged
 
-plt.show()
+    # 描画
+    plt.figure(figsize=(8, 8))
+
+    plt.imshow(
+        divergence_step,
+        extent = [xmin, xmax, ymin, ymax],
+        cmap = "twilight_shifted",
+        origin = "lower",
+        interpolation = "bicubic"
+    )
+
+    plt.colorbar(label="Iterations")
+
+    plt.title("Mandelbrot Set")
+    plt.xlabel("Re(c)")
+    plt.ylabel("Im(c)")
+
+    plt.show()
+
+mandelbrot(-0.75, 0.1, scale=10)
